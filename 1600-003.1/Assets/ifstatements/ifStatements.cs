e093d6af43f524ac89789e83b3fdb30d54c8b27a
unknown <10682823@CS514-15.ad.uvu.edu>
2016-01-27 10:00:58 -0700

    adding ifstatement

---
+++ 1600-003.1/Assets/ifstatements/ifStatements.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifStatements : MonoBehaviour 
+
+{
+	public float coffeeTemperture = 85.0f;
+	public float hotLimitTemperture = 70.0f;
+	public float coldLimitTemperture = 40.0f;
+
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TempertureTest();
+
+		coffeeTemperture -= Time.deltaTime * 5f;
+	}
+
+	void TempertureTest ()
+	{
+		if(coffeeTemperture > hotLimitTemperture)
+		{
+			print("Coffee is too hot.");
+		}
+
+		else if(coffeeTemperture < coldLimitTemperture)
+		{
+			print("Coffee is too cold.");
+		}
+
+		else
+		{
+			print("Coffee is perfect.");
+		}
+	}
+}
