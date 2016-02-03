1635390f51606db93d27ea771785aa01cceeca2a
unknown <10682823@CS514-15.ad.uvu.edu>
2016-02-03 09:55:48 -0700

    loops

---
+++ 1600-003.1/Assets/Loop/ForEachLoop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForEachLoop : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		string[] strings = new string[3];
+
+		strings [0] = "First string";
+		strings [1] = "Second string";
+		strings [2] = "Third strings";
+
+		foreach (string item in strings) 
+		{
+			print (item);
+		}
+	}
+}
+	
