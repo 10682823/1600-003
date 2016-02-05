717a9955b15c19507f372a9ff8a8a225a1a26d32
unknown <10682823@CS514-15.ad.uvu.edu>
2016-02-05 09:55:49 -0700

    Scope

---
+++ 1600-003.1/Assets/Scope/AnotherClass.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnotherClass : MonoBehaviour
+{
+	public int cheerios;
+	public int fruitloops;
+
+
+	private int cat;
+	private int dog;
+
+
+	public void Cereal (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log ("Cereal total: " + answer);
+	}
+
+	private void Animals (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log ("Animal total: " + answer);
+	}
+}
+
---
+++ 1600-003.1/Assets/Scope/ScopeAndAccessModifiers.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAndAccessModifiers : MonoBehaviour 
+{
+	public int alpha = 5;
+
+
+	private int beta = 0;
+	private int gamma = 5;
+
+	private AnotherClass myOtherClass;
+
+	void Start ()
+	{
+		alpha = 29;
+
+		myOtherClass = new AnotherClass();
+		myOtherClass.Cereal(alpha, myOtherClass.cheerios);
+	}
+
+	void Update ()
+	{
+		Debug.Log ("Alpha is set to: " + alpha);
+	}
+}
+
