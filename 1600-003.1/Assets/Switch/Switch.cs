7a469e560e1d98531235b87696e150149395f764
unknown <10682823@CS514-15.ad.uvu.edu>
2016-03-02 09:24:14 -0700

    switch

---
+++ 1600-003.1/Assets/Switch/Switch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConversationScript : MonoBehaviour 
+{
+	public int intelligence = 5;
+	
+	
+	void Greet()
+	{
+		switch (intelligence)
+		{
+		case 5:
+			print ("How goes it!");
+			break;
+		case 4:
+			print ("Hello There!");
+			break;
+		case 3:
+			print ("Watch out");
+			break;
+		case 2:
+			print ("BOOM!");
+			break;
+		case 1:
+			print ("POW");
+			break;
+		default:
+			print ("Incorrect.");
+			break;
+		}
+	}
+}
+
